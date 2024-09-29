5d3797d41c944c128216838ceb4d0a70e9a3de67
NP <mahdimazhabi@gmail.com>
2024-09-29 15:33:30 +0330

    rsisponsive project

--- src/components/Calender/Calender.jsx
+++ src/components/Calender/Calender.jsx
@@ -2,21 +2,25 @@ import img from "../../assets/Imgs/Calender/image.png";
 const Calender = () => {
   return (
     <section className="mt-12">
-      <div className=" flex justify-between items-center  px-[144px]">
-        <div>
-          <img src={img} alt="#" className="w-[442px] " />
+      <div className="flex flex-col lg:flex-row lg:justify-between justify-center items-center text-center lg:text-left px-5 lg:px-[144px]">
+        <div className="w-full lg:w-auto mb-8 lg:mb-0">
+          <img
+            src={img}
+            alt="#"
+            className="w-[80%] sm:w-[442px] mx-auto lg:mx-0"
+          />
         </div>
-        <div className="w-[661px]">
-          <h1 className="text-coustomZinkColors font-semibold text-4xl">
+        <div className="w-full lg:w-[661px]">
+          <h1 className="text-coustomZinkColors font-semibold text-2xl sm:text-3xl lg:text-4xl">
             How to design your site footer like we did
           </h1>
-          <p className="text-coustomGrayColors text-sm w-[601px] mt-4">
+          <p className="text-coustomGrayColors text-sm sm:text-base w-full sm:w-[601px] mx-auto lg:mx-0 mt-4">
             One event calendar shared across National, Regional and Club events
             to simplify your event experience. Allowing you to create waiting
             lists, share data capture, issue credential for course completion,
             apply smart purchasing rules and much more.
           </p>
-          <button className="w-[151px] h-[52px] bg-coustomGreenColors text-coustomwhiteColors mt-8 rounded">
+          <button className="w-[151px] h-[52px] bg-coustomGreenColors text-coustomwhiteColors mt-8 rounded mx-auto lg:mx-0">
             Learn More
           </button>
         </div>
--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -16,14 +16,22 @@ const Customers = () => {
     { id: 5, img: icon5 },
     { id: 6, img: icon6 },
   ];
+
   return (
     <section>
-      <div className="flex justify-between items-center px-[144px] py-[33px] bg-coustomwhiteColors space-x-20">
-        <div>
-          <img src={Img} alt="Img Tesla" className="w-[326px]" />
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center lg:px-[144px] px-5 py-[33px] bg-coustomwhiteColors lg:space-x-20">
+        {/* Image Section */}
+        <div className="mb-8 lg:mb-0">
+          <img
+            src={Img}
+            alt="Img Tesla"
+            className="w-[80%] sm:w-[326px] mx-auto"
+          />
         </div>
-        <div className="w-[748px]">
-          <p className="text-coustomZinkColors text-base ">
+
+        {/* Text Section */}
+        <div className="lg:w-[748px] text-center lg:text-left">
+          <p className="text-coustomZinkColors text-sm sm:text-base">
             “We have enjoyed working with the JustGo system to setup and go live
             with our new membership platform. The initial effort to organise it
             along the lines of how our sport currently works was quick and
@@ -33,13 +41,15 @@ const Customers = () => {
             sport in a much more effective way, and collect data to help us
             improve how our sport is governed.”
           </p>
-          <span className="text-coustomGreenColors text-lg font-semibold">
+          <span className="text-coustomGreenColors text-lg font-semibold block mt-4">
             Tim Smith
           </span>
-          <p className="mt-4 text-coustomGrayColors text-base">
+          <p className="mt-2 text-coustomGrayColors text-sm sm:text-base">
             British Dragon Boat Racing Association
           </p>
-          <div className="flex items-center space-x-[41px] mt-8">
+
+          {/* Icons and Call to Action */}
+          <div className="flex flex-wrap justify-center lg:justify-start items-center  lg:space-x-[41px] space-y-4 lg:space-y-0 mt-8">
             {Icons.map((Index) => (
               <img
                 key={Index.id}
@@ -48,11 +58,11 @@ const Customers = () => {
                 className="w-[48px] h-[48px]"
               />
             ))}
-            <div className="flex items-center space-x-2 cursor-pointer ">
+            <div className="flex items-center space-x-2 cursor-pointer mt-4 lg:mt-0">
               <span className="text-base font-semibold text-coustomGreenColors">
                 Meet all customers
               </span>
-              <MdArrowRightAlt className="w-6 h-6 relative top-0.5 text-coustomGreenColors" />
+              <MdArrowRightAlt className="w-6 h-6 text-coustomGreenColors" />
             </div>
           </div>
         </div>
